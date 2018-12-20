88d24a4bca8ce2fc9175c880b2f18ad1d4352b87
CG1507 <g8ghanshym@gmail.com>
2018-12-20 13:28:21 +0530

    :tada: updated sklearn_lr.py

--- DAY1/code/linear_regressor/sklearn_lr.py
+++ DAY1/code/linear_regressor/sklearn_lr.py
@@ -6,20 +6,47 @@ from sklearn import linear_model
 import pandas as pd
 
 # Load CSV and columns for Housing.csv
+df = pd.read_csv('Housing.csv')
+X = df['plotsize']
+bedrooms = df['bedrooms']
+Y = df['price']
+
 # Get numpy array
+X = np.array(X)
+bedrooms = np.array(bedrooms)
+Y = np.array(Y)
+
 # Reshape data into (row, 1)
+X = X.reshape(len(X), 1)
+bedrooms = bedrooms.reshape(len(bedrooms), 1)
+Y = Y.reshape(len(Y), 1)
+
 # Join data column into with axis=1
+X = np.append(X, bedrooms, axis=1)
+
 # Split dataset in train/test
+X_train = X[:X.shape[0]*80//100, :]
+Y_train = Y[:Y.shape[0]*80//100, :]
+
+X_test = X[X.shape[0]*80//100:, :]
+Y_test = Y[Y.shape[0]*80//100:, :]
+
 # Get sklearn model = linear_model.LinearRegression()
+model = linear_model.LinearRegression()
+
 # Start training
+print("Start Training...")
+model.fit(X_train, Y_train)
+
 # Predict
+y_pred = model.predict(X_test)
 
 
-
-
-
-
-
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X_test[:, 0], X_test[:, 1], Y_test[:,0])
+ax.plot(X_test[:, 0], X_test[:, 1], y_pred[:,0], color='red',linewidth=3)
+plt.show()
 
 
 
